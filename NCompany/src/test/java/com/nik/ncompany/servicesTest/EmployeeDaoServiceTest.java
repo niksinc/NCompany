d1445856779df18bc3d3e273156ab79c28b121f4
niksinfinite10 <nikhil.surati@gmail.com>
2016-04-05 09:38:50 -0700

    Service Class Test Updated
    Employee Service Class Test is For the Bad Email insert

---
+++ NCompany/src/test/java/com/nik/ncompany/servicesTest/EmployeeDaoServiceTest.java
@@ -0,0 +1,52 @@
+package com.nik.ncompany.servicesTest;
+
+import static org.junit.Assert.*;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nik.ncompany.DaojdbcTest.EmplpoyeeDaoJdbcTest;
+import com.nik.ncompany.domain.Employee;
+import com.nik.ncompany.services.EmployeeDaoService;
+import com.nik.ncompany.services.EmployeeDaoServiceImpl;
+
+@ContextConfiguration("classpath:ncompany-test-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@Transactional
+@TransactionConfiguration
+public class EmployeeDaoServiceTest {
+	@Autowired
+	@Qualifier("employeeDaoServiceImpl")
+	private EmployeeDaoServiceImpl employeeDaoServiceImpl;
+	private Logger logger =  Logger.getLogger(EmplpoyeeDaoJdbcTest.class);
+	
+	/* This test will demonstrate the handling of unchecked exception */
+	@Test 
+	public void testBadEmailInsert(){
+		int empCount = employeeDaoServiceImpl.getEmployeeCount();
+				
+		Employee emp = new Employee ("Ram","surti",null,"741852963");
+		try{
+			employeeDaoServiceImpl.insertEmployee(emp );
+			fail();
+		}
+		catch(DataIntegrityViolationException ex){
+			
+			/* The Employee insert will not have rollback 
+			 * because we have a checked exception */
+			
+			logger.info("Employee isert failed becouse exception"+ex);
+		}
+		assertTrue(empCount == employeeDaoServiceImpl.getEmployeeCount());
+	}
+
+}
